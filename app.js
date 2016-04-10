e56512152935a16e675095899d306aecc84d1631
Maksim Murygin <muriginm@gmail.com>
2016-04-10 12:45:53 +0600

    implemented middlewares

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bodyParser = require('body-parser');
+var multer = require('multer');
+const flash = require('connect-flash');
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const expressValidator = require('express-validator');
+const expressMessages = require('express-messages');
+
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
 var app = express();
 
@@ -14,13 +25,52 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Handle File Uploads
+app.use(multer({ dest: './uploads/'}).any() );
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handle Express Sessions
+app.use(session({
+  secret: 'lsdfjasdf_%dfss',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.');
+    var root    = namespace.shift();
+    var formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = expressMessages(req, res);
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
